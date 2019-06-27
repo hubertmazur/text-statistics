dba43ab64a976a5032bb75d5c3483a7f0d99a86d
hubert <hubert.mazur@vp.pl>
2019-06-27 18:44:48 +0200

    adding an interface implementation

--- src/main/java/Statistics.java
+++ src/main/java/Statistics.java
@@ -1,2 +1,22 @@
-public interface Statistics {
+import java.util.Map;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+
+public interface Statistics<T> {
+
+    T analyze(String text);
+
+    String analyzeWithFormattedOutput(String text);
+
+    static Map<String,Long> getWordsMap(String text) {
+        return Arrays.stream(text
+                .toLowerCase()
+                .split("[/. -]|\\r?\\n"))
+                .map(x->x.replaceAll("[^\\p{LD}\\s]",""))
+                .filter(x->!x.matches(""))
+                .collect(Collectors.groupingBy(x->x, Collectors.counting()));
+    }
+
+
 }
