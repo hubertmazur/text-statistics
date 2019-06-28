702ea238b44af1905607ca37c8c200095f2c0841
hubert <hubert.mazur@vp.pl>
2019-06-28 21:36:31 +0200

    Add other method to count words in text.

--- src/main/java/WordCountStatistic.java
+++ src/main/java/WordCountStatistic.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+
 public class WordCountStatistic implements Statistics <Long>  {
 
     @Override
@@ -9,8 +11,17 @@ public class WordCountStatistic implements Statistics <Long>  {
                 .orElseThrow(IllegalArgumentException::new);
     }
 
+
     @Override
     public String analyzeWithFormattedOutput(String text) {
         return String.format("Tekst zawiera %d słów", analyse(text));
+    }
+    public  Long analyse2 (String text) {
+        Map<String,Long> wordsmap = Statistics.getWordsMap(text) ;
+        long sumWords = 0;
+        for (Long values : wordsmap.values()) {
+            sumWords+= values ;
+        }
+        return sumWords;
     }
 }
